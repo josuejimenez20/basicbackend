d1d41f60851137b3f539bc172392d5bab73a2d22
Josue Jimenez <josuejimenez20@gmail.com>
2024-10-04 10:50:16 -0600

    New Version V9

--- backend/src/models/users/usersModels.js
+++ backend/src/models/users/usersModels.js
@@ -17,7 +17,7 @@ function getInformationUserModels() {
     //         })
     // })
 
-    return "Hello from users models new version v3"
+    return "Hello from users models new version v4"
 }
 
 
